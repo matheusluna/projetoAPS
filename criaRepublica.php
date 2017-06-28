d22ebf59c6b3f79449143a62aa485d2835151f61
Thiago Yure Vieira de Almeida <thigoyure@gmail.com>
2017-06-28 15:47:05 -0300

    Acredito estar feito a criação de república, vou testar...

---
+++ criaRepublica.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<script type="text/javascript" src='sweetalert/dist/sweetalert.min.js'></script>
+    <link rel='stylesheet' type='text/css' href='sweetalert/dist/sweetalert.css'>
+</head>
+<body>
+
+	<?php
+		session_start();
+		
+		include("crudMySql.php");
+
+		$nome = $_POST['nome'];
+		$endereco = $_POST['endereco'];
+		$vagas = $_POST['vagas'];
+		$contato = $_POST['contato'];
+		$cidade = $_POST['cidade'];
+		$gerente = $_SESSION['email'];
+
+		if (!empty($cidade)&&!empty($nome)&&!empty($contato)) {
+			if (read_database('republica', "WHERE nome = '$nome'") == FALSE) {
+
+				$data = array('nome' => '$nome', 'endereco' => '$endereco','vagas' => '$vagas','contato' => '$contato','cidade' => '$cidade','gerente' => '$gerente' );
+				if(create_database('republica',$data) == TRUE){
+					echo "<script>
+        sweetAlert('Sucesso na criação da república', 'República criada com sucesso', 'success');
+            </script>";
+				}else{
+					echo "<script>
+        sweetAlert('Falha na criação da república', 'Falha ao criar república', 'error');
+            </script>";
+				}
+				
+			}else{
+				echo "<script>
+        sweetAlert('República já em sistema', 'A república já existe', 'error');
+            </script>";
+			}
+		}else{
+			echo "<script>
+        sweetAlert('Dados vazios', 'Preencha os campos vazios!', 'error');
+            </script>";
+			}
+		}
+	?>
+
+</body>
+</html>
