eb2ecd4f221c12bc1d95201663a01d9e2c2368e6
RomuloSoaresBezerra <bromulosoares@gmail.com>
2017-07-05 16:41:00 -0300

    atualização de usuário para gerente ao criar republica adicionada

--- criaRepublica.php
+++ criaRepublica.php
@@ -5,54 +5,105 @@
     <link rel='stylesheet' type='text/css' href='sweetalert/dist/sweetalert.css'>
   </head>
   <body>
-	
+	<?php include("bloqueiaAcessoDiretoURL.php");?>
   </body>
 </html>
-<?php
-  
-  include("conexao.php");
-  $conexao = open_database();
-  if ($conexao != null) {
-    if($_POST){
-      $nome = $_POST['nome'];
-      $rua = $_POST['rua'];
-      $bairro = $_POST['bairro'];
-      $vagas = (int) $_POST['vagas'];
-      $contato = $_POST['contato'];
-      $cidade = $_POST['cidade'];
-      $gerente = $_SESSION['email'];
-      $sql = "INSERT INTO republica (nome,rua,bairro,numerovagas,contato,cidade,gerente) VALUES ('$nome','$rua','$bairro','$vagas','$contato','$cidade','$gerente')";
-      $sql2 = "SELECT * FROM republica WHERE nome = '$nome'";
-      
-      if(empty($cidade)||empty($nome)||empty($rua)||empty($vagas)||empty($contato)||empty($gerente)||empty($bairro)){
-        echo "<script>
-         sweetAlert('Dados vazios ou incorretos', 'Preencha os campos vazios ou corrija os erros!', 'error');
-              </script>";
 
-      }else{
-        if (mysqli_query($conexao, $sql2)->num_rows==0){
-          if(mysqli_query($conexao, $sql)){
-           echo "<script>
-          sweetAlert('Cadastro Realizado', 'República criada!', 'success');
-          setTimeout(function() { location.href='gerenciarepublica.html' }, 1000);
-              </script>";
-          }else{
-            echo "<script>
-          sweetAlert('Falha ao criar', 'Houve um erro! A república não foi criada...', 'error');
-              </script>";
-          }
-        }else{
-          echo "<script>
-          sweetAlert('Falha ao criar república', 'Já existe uma república com esse nome!', 'error');
-             </script>";
-        }
-      }
-    }
-    mysqli_close($conexao);
+<?php
+  	//session_start(); A sessão já está aberta na linha do body acima
+  	include("conexao.php");
+	$conexao = open_database();
+	if ($conexao != null) {
+		if($_POST){
+	      $nome = $_POST['nome'];
+	      $rua = $_POST['rua'];
+	      $bairro = $_POST['bairro'];
+	      $vagas = (int) $_POST['vagas'];
+	      $contato = $_POST['contato'];
+	      $cidade = $_POST['cidade'];
+	      $gerente = $_SESSION['email'];
+		  
+	      $sql = "INSERT INTO republica (nome, rua, bairro, numerovagas, contato, cidade, gerente) ". 
+	      		  "VALUES ('$nome', '$rua', '$bairro', '$vagas', '$contato', '$cidade', '$gerente')";
+	      $sql2 = "SELECT * FROM republica WHERE nome = '$nome'";
+	      
+	      if(empty($cidade)||empty($nome)||empty($rua)||empty($vagas)||empty($contato)||empty($gerente)||empty($bairro)){
+	      	echo "<script>
+	        sweetAlert('Dados vazios ou incorretos', 'Preencha os campos vazios ou corrija os erros!', 'error');
+	        </script>";
+	
+	      }else{
+			if (mysqli_query($conexao, $sql2)->num_rows == 0){
+	        		
+	        	//Desabilita autocommit
+	        	mysqli_autocommit($conexao, FALSE);
+				
+	        	if(mysqli_query($conexao, $sql)){
+	            		
+	            	//insere o commit
+	            	mysqli_commit($conexao);
+					
+	            	$repCriada = TRUE;
+	              	
+				  	//------ Trecho atualiza usuario para gerente
+				  	$sqlConsultaUser = "SELECT * FROM usuario WHERE email = '$gerente'";
+				  	
+				  	$result = mysqli_query($conexao, $sqlConsultaUser);
+				  	if(mysqli_num_rows($result) == 1){
+				  		$row = mysqli_fetch_assoc($result);
+						
+						$sqlAtualizaUser = "UPDATE usuario ".
+										   "SET tipo='gerente', nomerepublica='$nome' ".
+										   "WHERE email = '$gerente'"; 
+										   
+						//Desabilita autocommit
+	        			mysqli_autocommit($conexao, FALSE);
+	        			
+						if(mysqli_query($conexao, $sqlAtualizaUser)){
+								
+							//insere o commit
+	            			mysqli_commit($conexao);
+							
+							$userAtualizado = TRUE;
+						}else{$userAtualizado = FALSE;}
+				  	}//------ Fim do trecho
+				  
+	          	}else{
+	          		$repCriada = FALSE;
+	            	echo "<script>
+	          		sweetAlert('Falha ao criar', 'Houve um erro! A república não foi criada...', 'error');
+	              	</script>";
+	          	}
+				
+				//Verifica a criação da republica e atualização do usuario para gerente 
+				//e volta um ou mais estados anteriores do banco em caso de erro
+				if($repCriada and $userAtualizado){
+					echo "<script> sweetAlert('Cadastro Realizado', 'República criada!', 'success'); 
+	          		setTimeout(function() { location.href='gerenciarepublica.php' }, 3000); </script>";
+				}else if(!$repCriada and !$userAtualizado){
+					mysqli_rollback($conexao);
+					mysqli_rollback($conexao);
+					echo "<script>
+	          		sweetAlert('Falha ao criar', 'Houve um erro! A república não foi criada...', 'error');
+	              	</script>";
+				}else if(!$repCriada or !$userAtualizado){
+					mysqli_rollback($conexao);
+					echo "<script>
+	          		sweetAlert('Falha ao criar', 'Houve um erro! A república não foi criada...', 'error');
+	              	</script>";
+	        	}
+	      }else{
+	        	echo "<script>
+	          	sweetAlert('Falha ao criar república', 'Já existe uma república com esse nome!', 'error');
+	            </script>";
+	        }
+	    }
+	    mysqli_close($conexao);
+	}
   }else{
-    echo "<script>
-          sweetAlert('Falha na conexão', 'Não foi possível se conectar ao servidor...', 'error');
-             </script>";
+		echo "<script>
+	    sweetAlert('Falha na conexão', 'Não foi possível se conectar ao servidor...', 'error');
+	    </script>";
   }
   
 ?>
@@ -73,7 +124,7 @@
     </head>
 
     <body class="grey lighten-4">
-      <?php include("bloqueiaAcessoDiretoURL.php");?>
+      
       <!--Import jQuery before materialize.js-->
       <ul id="slide-out" class="side-nav">
         <li><div class="userView">
