66d025e119d4f67db800bc244f7dabf88caa781d
Lyndemberg <lyndembergbatista@outlook.com>
2017-08-09 23:37:53 -0300

    Enviando tela de mural

---
+++ mural.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<!--Import Google Icon Font-->
+	<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<!--Import materialize.css-->
+	<link type="text/css" rel="stylesheet" href="materialize/css/materialize.min.css" media="screen,projection" />
+
+	<!--Let browser know website is optimized for mobile-->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title></title>
+</head>
+
+<body>
+	<div class="container">
+
+		<center>
+			<h3>Mural de avisos</h3>
+			<!-- ABRE JANELA DE CRIAÇÃO -->
+
+		</center>
+		<a class="modal-trigger btn-floating btn-small waves-effect waves-light red" href="#janelaAviso"><i class="material-icons">add</i></a>
+		<label class="" style="font-size:15pt">Criar novo</label><br><br>
+
+
+		<!-- LISTA TODOS OS AVISOS -->
+		<ul class="collapsible" data-collapsible="accordion">
+			<li>
+				<div class="collapsible-header">Título 1<span class="badge"><a class="small material-icons" href="excluirAviso.php?id=124">cancel</a></span></div>
+				<div class="collapsible-body">
+					<p>Conteudo 1 aqui</p>
+				</div>
+			</li>
+			<li>
+				<div class="collapsible-header">Título 2<span class="badge"><a class="small material-icons" href="excluirAviso.php?id=125">cancel</a></span></div>
+				<div class="collapsible-body">
+					<p>Conteudo 2 aqui</p>
+				</div>
+			</li>
+		</ul>
+
+		<!-- CONTEÚDO DA JANELA DE CRIAÇÃO -->
+		<div id="janelaAviso" class="modal">
+			<div class="modal-content">
+				<h4>Novo aviso</h4>
+				<form method="post" action="novoAviso.php">
+					<label for="tituloAviso">Título</label>
+					<input placeholder="Digite o título aqui" id="tituloAviso" name="tituloAviso" type="text" class="validate">
+					<label for="descricaoAviso">Descrição</label>
+					<textarea id="descricaoAviso" name="descricaoAviso" class="materialize-textarea"></textarea>
+					<button class="btn waves-effect waves-light right" type="submit">Enviar
+    					<i class="material-icons right">send</i>
+  					</button>
+				</form>
+				<br><br>
+			</div>
+		</div>
+
+
+	</div>
+
+	<!--Import jQuery before materialize.js-->
+	<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+	<script type="text/javascript" src="materialize/js/materialize.min.js"></script>
+	<script type="text/javascript">
+		$(document).ready(function() {
+
+			$('.modal').modal();
+		});
+
+	</script>
+</body>
+
+</html>
