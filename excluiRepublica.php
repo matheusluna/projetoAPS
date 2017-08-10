8fea4109ba84fb6355e403273fd16ed6b7760508
Jozimar <jozimarsoares.js@gmail.com>
2017-08-10 08:09:02 -0300

    Excluir República Adicionado

---
+++ excluiRepublica.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Ecluir República</title>
+    <link rel='stylesheet' type='text/css' href='sweetalert/dist/sweetalert.css'>
+  </head>
+  <body>
+  <script type="text/javascript" src='sweetalert/dist/sweetalert.min.js'></script>
+  </body>
+</html>
+
+<?php
+      include("conexao.php");
+      $conexao = open_database();
+
+      if($conexao != null){
+
+      $republica = $_GET['nomerepublica'];
+      $email = $_GET['usuario'];
+
+			$sqlDelete = "DELETE FROM republica WHERE gerente = '$email' and
+					      nome = '$republica'";
+
+			if(mysqli_query($conexao, $sqlDelete)){
+
+          $sqlAtualizaUser = "UPDATE usuario ".
+                   "SET tipo='null'".
+                   "WHERE email = '$email'";
+
+        mysqli_query($conexao, $sqlAtualizaUser);
+
+				echo "<script>
+					  		swal('República Deletada!', 'Sua República foi Deletada!', 'success');
+							setTimeout(function() { location.href='minhaRepublicaGerente.php' }, 3000);
+					  </script>";
+
+			}
+			else echo "<script>
+							swal('Erro ao Deletar!', 'Não foi possível deletar sua república!', 'error');
+							setTimeout(function() { location.href='minhaRepublicaGerente.php' }, 3000);
+					  </script>";
+
+		}
+
+  	mysqli_close($conexao);
+
+?>
--- minhaRepublicaGerente.php
+++ minhaRepublicaGerente.php
@@ -127,7 +127,9 @@
       </div>
 </form>
         <a class="btn" href="#modal1">Editar</a>
-        <button class="btn" type="button" name="button">Excluir</button>
+        <?php
+        echo "<a class='btn' href='excluiRepublica.php?nomerepublica=$nomeRepublica&usuario=$email'>Excluir</a>";
+         ?>
       </div>
       <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
       <script type="text/javascript">
